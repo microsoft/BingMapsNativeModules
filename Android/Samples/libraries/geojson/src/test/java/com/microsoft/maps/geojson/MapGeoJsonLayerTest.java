f8094ff3c25e23747e93c5ab286880c4ea05b2c1
Elizabeth Bartusiak <t-elbart@microsoft.com>
2020-06-09 15:40:30 -0500

    Added strokeColor and strokeDashed.

--- Android/Samples/libraries/geojson/src/test/java/com/microsoft/maps/geojson/MapGeoJsonLayerTest.java
+++ Android/Samples/libraries/geojson/src/test/java/com/microsoft/maps/geojson/MapGeoJsonLayerTest.java
@@ -1,10 +1,13 @@
 package com.microsoft.maps.geojson;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import android.graphics.Color;
 import com.microsoft.maps.MapPolygon;
+import com.microsoft.maps.MapPolyline;
 import com.microsoft.maps.MockBingMapsLoader;
 import com.microsoft.maps.MockMapElementCollection;
 import org.junit.Before;
@@ -30,6 +33,18 @@ public class MapGeoJsonLayerTest {
     assertEquals(1, elementCollection.getElements().size());
     MapPolygon polygon = (MapPolygon) elementCollection.getElements().get(0);
     assertNotNull(polygon);
+  }
+
+  @Test
+  public void testAddPolyline() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    MapPolyline poly = MOCK_MAP_FACTORIES.createMapPolyline();
+    layer.getElements().add(poly);
+    MockMapElementCollection elementCollection = (MockMapElementCollection) layer.getElements();
+    assertNotNull(elementCollection);
+    assertEquals(1, elementCollection.getElements().size());
+    MapPolyline polyline = (MapPolyline) elementCollection.getElements().get(0);
+    assertNotNull(polyline);
   }
 
   @Test
@@ -77,6 +92,90 @@ public class MapGeoJsonLayerTest {
       MapPolygon polygon = (MapPolygon) elementCollection.getElements().get(0);
       assertNotNull(polygon);
       assertEquals(Color.GREEN, polygon.getFillColor());
+    }
+  }
+
+  @Test
+  public void testGetStrokeColorDefault() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    assertEquals(Color.BLUE, layer.getStrokeColor());
+  }
+
+  @Test
+  public void testSetStrokeColor() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    MapPolygon polygonAdd = MOCK_MAP_FACTORIES.createMapPolygon();
+    layer.getElements().add(polygonAdd);
+    MapPolyline polylineAdd = MOCK_MAP_FACTORIES.createMapPolyline();
+    layer.getElements().add(polylineAdd);
+    MockMapElementCollection elementCollection = (MockMapElementCollection) layer.getElements();
+    assertNotNull(elementCollection);
+    assertEquals(2, elementCollection.getElements().size());
+    layer.setStrokeColor(Color.GREEN);
+    assertEquals(Color.GREEN, layer.getStrokeColor());
+
+    MapPolygon polygonReceived = (MapPolygon) elementCollection.getElements().get(0);
+    assertNotNull(polygonReceived);
+    assertEquals(Color.GREEN, polygonReceived.getStrokeColor());
+
+    MapPolyline polylineReceived = (MapPolyline) elementCollection.getElements().get(1);
+    assertNotNull(polylineReceived);
+    assertEquals(Color.GREEN, polylineReceived.getStrokeColor());
+  }
+
+  @Test
+  public void testSetStrokeColorMultiplePolygonsPolylines() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    for (int i = 0; i < 2; i++) {
+      MapPolygon polygonAdd = MOCK_MAP_FACTORIES.createMapPolygon();
+      layer.getElements().add(polygonAdd);
+      MapPolyline polylineAdd = MOCK_MAP_FACTORIES.createMapPolyline();
+      layer.getElements().add(polylineAdd);
+    }
+    MockMapElementCollection elementCollection = (MockMapElementCollection) layer.getElements();
+    assertNotNull(elementCollection);
+    assertEquals(4, elementCollection.getElements().size());
+    layer.setStrokeColor(Color.GREEN);
+    assertEquals(Color.GREEN, layer.getStrokeColor());
+    for (int i = 0; i < 4; i += 2) {
+      MapPolygon polygon = (MapPolygon) elementCollection.getElements().get(i);
+      assertNotNull(polygon);
+      assertEquals(Color.GREEN, polygon.getStrokeColor());
+
+      MapPolyline polyline = (MapPolyline) elementCollection.getElements().get(i + 1);
+      assertNotNull(polyline);
+      assertEquals(Color.GREEN, polygon.getStrokeColor());
+    }
+  }
+
+  @Test
+  public void testGetStrokeDashedDefault() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    assertFalse(layer.getStrokeDashed());
+  }
+
+  @Test
+  public void testSetStrokeDashedPolygonsPolylines() {
+    MapGeoJsonLayer layer = MOCK_MAP_FACTORIES.createMapGeoJsonLayer();
+    for (int i = 0; i < 2; i++) {
+      MapPolygon polygonAdd = MOCK_MAP_FACTORIES.createMapPolygon();
+      layer.getElements().add(polygonAdd);
+      MapPolyline polylineAdd = MOCK_MAP_FACTORIES.createMapPolyline();
+      layer.getElements().add(polylineAdd);
+    }
+    MockMapElementCollection elementCollection = (MockMapElementCollection) layer.getElements();
+    assertNotNull(elementCollection);
+    assertEquals(4, elementCollection.getElements().size());
+    layer.setStrokeDashed(true);
+    assertTrue(layer.getStrokeDashed());
+    for (int i = 0; i < 4; i += 2) {
+      MapPolygon polygon = (MapPolygon) elementCollection.getElements().get(i);
+      assertNotNull(polygon);
+      assertTrue(polygon.isStrokeDashed());
+
+      MapPolyline polyline = (MapPolyline) elementCollection.getElements().get(i + 1);
+      assertNotNull(polyline);
+      assertTrue(polyline.isStrokeDashed());
     }
   }
 }
--- Android/Samples/libraries/geojson/src/test/java/com/microsoft/maps/geojson/MockGeoJsonLayerMapFactories.java
+++ Android/Samples/libraries/geojson/src/test/java/com/microsoft/maps/geojson/MockGeoJsonLayerMapFactories.java
@@ -41,6 +41,51 @@ class MockGeoJsonLayerMapFactories implements MapFactories {
         .setFillColor(Mockito.anyInt());
 
     doAnswer(invocation -> fillColor.get()).when(layer).getFillColor();
+
+    final AtomicReference<Integer> strokeColor = new AtomicReference();
+    // set the default value
+    strokeColor.set(Color.BLUE);
+    doAnswer(
+            invocation -> {
+              int color = invocation.getArgument(0);
+              strokeColor.set(color);
+
+              for (MapElement element : mockElementCollection.getElements()) {
+                if (element instanceof MapPolygon) {
+                  ((MapPolygon) element).setStrokeColor(color);
+                } else if (element instanceof MapPolyline) {
+                  ((MapPolyline) element).setStrokeColor(color);
+                }
+              }
+              return true;
+            })
+        .when(layer)
+        .setStrokeColor(Mockito.anyInt());
+
+    doAnswer(invocation -> strokeColor.get()).when(layer).getStrokeColor();
+
+    final AtomicReference<Boolean> isStrokeDashed = new AtomicReference();
+    // set the default value
+    isStrokeDashed.set(false);
+    doAnswer(
+            invocation -> {
+              boolean arg = invocation.getArgument(0);
+              if (isStrokeDashed.get() != arg) {
+                isStrokeDashed.set(arg);
+                for (MapElement element : mockElementCollection.getElements()) {
+                  if (element instanceof MapPolygon) {
+                    ((MapPolygon) element).setStrokeDashed(arg);
+                  } else if (element instanceof MapPolyline) {
+                    ((MapPolyline) element).setStrokeDashed(arg);
+                  }
+                }
+              }
+              return true;
+            })
+        .when(layer)
+        .setStrokeDashed(Mockito.anyBoolean());
+
+    doAnswer(invocation -> isStrokeDashed.get()).when(layer).getStrokeDashed();
     return layer;
   }
 
@@ -57,15 +102,39 @@ class MockGeoJsonLayerMapFactories implements MapFactories {
 
   @Override
   public MapPolyline createMapPolyline() {
-    return null;
+    MapPolyline polyine = Mockito.mock(MapPolyline.class);
+
+    final AtomicReference<Integer> strokeColor = new AtomicReference();
+    // set the default value
+    strokeColor.set(Color.BLUE);
+    doAnswer(
+            invocation -> {
+              strokeColor.set(invocation.getArgument(0));
+              return true;
+            })
+        .when(polyine)
+        .setStrokeColor(Mockito.anyInt());
+
+    doAnswer(invocation -> strokeColor.get()).when(polyine).getStrokeColor();
+
+    final AtomicReference<Boolean> isStrokeDashed = new AtomicReference();
+    doAnswer(
+            invocation -> {
+              isStrokeDashed.set(invocation.getArgument(0));
+              return true;
+            })
+        .when(polyine)
+        .setStrokeDashed(Mockito.anyBoolean());
+
+    doAnswer(invocation -> isStrokeDashed.get()).when(polyine).isStrokeDashed();
+
+    return polyine;
   }
 
   @Override
   public MapPolygon createMapPolygon() {
     MapPolygon polygon = Mockito.mock(MapPolygon.class);
 
-    // fillColor must be final for use within the 'doAnswer' clause
-    // AtomicReference used as a wrapper class to capture changes to fillColor
     final AtomicReference<Integer> fillColor = new AtomicReference();
     // set the default value
     fillColor.set(Color.BLUE);
@@ -78,6 +147,31 @@ class MockGeoJsonLayerMapFactories implements MapFactories {
         .setFillColor(Mockito.anyInt());
 
     doAnswer(invocation -> fillColor.get()).when(polygon).getFillColor();
+
+    final AtomicReference<Integer> strokeColor = new AtomicReference();
+    // set the default value
+    strokeColor.set(Color.BLUE);
+    doAnswer(
+            invocation -> {
+              strokeColor.set(invocation.getArgument(0));
+              return true;
+            })
+        .when(polygon)
+        .setStrokeColor(Mockito.anyInt());
+
+    doAnswer(invocation -> strokeColor.get()).when(polygon).getStrokeColor();
+
+    final AtomicReference<Boolean> isStrokeDashed = new AtomicReference();
+    doAnswer(
+            invocation -> {
+              isStrokeDashed.set(invocation.getArgument(0));
+              return true;
+            })
+        .when(polygon)
+        .setStrokeDashed(Mockito.anyBoolean());
+
+    doAnswer(invocation -> isStrokeDashed.get()).when(polygon).isStrokeDashed();
+
     return polygon;
   }
 }
